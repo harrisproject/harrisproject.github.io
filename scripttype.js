b2b17d51ebdeaa1e118ae3379774394b88386212
harrisproject <41648114+harrisproject@users.noreply.github.com>
2018-07-30 12:21:34 -0700

    Add files via upload

---
+++ scripttype.js
@@ -0,0 +1,14 @@
+var scripttype = {
+    alphabet: `
+    <a class='scripthead' target='_blank' href='https://en.wikipedia.org/wiki/Alphabet'>Alphabets</a>
+    <div class='scrblock'>
+        <div class='letblock letters v' style='width:auto'>Vowels are colored pink</div>
+        <div class='letblock letters c' style='width:auto'>Consonants are colored red</div>
+        <div class='letblock letters o' style='width:auto'>Other letters are colored orange</div>
+    </div>`,
+    abjad: `
+    <a class='scripthead' target='_blank' href='https://en.wikipedia.org/wiki/Abjad'>Abjads</a>
+    <div class='scrblock'>
+        <div class='letblock vowels' style='width:auto'>Diacritics, which are optional in these scripts, are colored purple</div>
+    </div>`
+};
